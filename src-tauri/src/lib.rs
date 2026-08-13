bf400718d4c20fa85462fbb8225f865b48e6fba9
Pavel <184332183+pavel-duke@users.noreply.github.com>
2026-08-13 20:35:09 +0300

    fix: сокращаю разрешения и обновляю иконку

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,7 +12,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             commands::workspace::create_workspace,
             commands::workspace::open_workspace,
